6cdaad678db877b6e78c6b2eada59278bd4aa5b1
Alexandra Dubovskaya <93725496+AlexandraDub@users.noreply.github.com>
2022-04-06 22:26:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -57,11 +57,12 @@ function createCalendar() {
     for (let i = 1; i <= daysCount; i++) {
         let dates = document.createElement('div');
         dates.textContent = i;
-        if (i === today.getDate()) {
+        if (i === today.getDate() &&
+            nameOfCurrentMonth.innerText == monthNames[today.getMonth()] + ' ' + today.getFullYear()
+        ) {
             dates.className = 'today_calendar_cell';
         } else dates.className = 'calendar_cell';
         containerForDates.append(dates);
-        dates.addEventListener("click", addNewTask);
     }
     
 }
@@ -76,21 +77,77 @@ function updateCalendarPrevious(){
 }
 
 createCalendar();
+
+
 // Добавление и удаление заметок
 const addTasks = document.getElementById('create_new_task');
-addTasks.addEventListener("click", addNewTask); 
-const fieldForNote = document.querySelector('.add_tasks');
+const placeForTasks = document.getElementById('calendar');
 
-function addNewTask() {
-let dayWithTaskDate = document.querySelector('.notes_head_date');
-dayWithTaskDate.textContent = '';
-// let clickarea = document.querySelector('calendar_cell');
-// clickarea.addEventListener('click', event => {
-//     dayWithTaskDate.textContent = nameOfCurrentMonth + ', ' + event.target.textContent;
-// })
+containerForDates.addEventListener('click', createTaskList);
 
-let newTask = document.createElement("textarea");
 
-fieldForNote.append(newTask);
-newTask.className = 'input_for_new_task';
-}
+function createTaskList(event) {
+    const elementCliked = event.target;
+    console.log(elementCliked);
+    console.log(elementCliked.className);
+    if (elementCliked.className === "calendar_cell" || elementCliked.className === "today_calendar_cell")
+    {
+        elementCliked.className = 'cliked_cell'
+        let uniqueId = elementCliked.innerText + ' ' + nameOfCurrentMonth.innerText;
+
+        console.log(uniqueId);
+        
+        let notesContainer = document.createElement('div');
+        notesContainer.className = 'notes_container';
+        placeForTasks.append(notesContainer);
+        
+        let notesContainerHeader = document.createElement('header');
+        notesContainerHeader.className = 'notes_head';
+        notesContainer.append(notesContainerHeader);
+
+        let headerName = document.createElement('div');
+        headerName.className = 'notes_head_date';
+        headerName.id = uniqueId;
+        headerName.textContent = uniqueId;
+        notesContainerHeader.append(headerName);
+
+        let buttonCreator = document.createElement('button');
+        buttonCreator.className = 'tasks_buttons';
+        buttonCreator.id = 'create_new_task';
+        buttonCreator.textContent = 'Add new task';
+        notesContainer.append(buttonCreator);
+
+        let tasksHolder = document.createElement('div');
+        tasksHolder.className = 'add_tasks';
+        notesContainer.append(tasksHolder);
+
+        let buttonSaver = document.createElement('button');
+        buttonSaver.className = 'tasks_buttons';
+        buttonSaver.id = 'save_task';
+        buttonSaver.textContent = 'Save task';
+        notesContainer.append(buttonSaver);
+
+        let buttonDeleter = document.createElement('button');
+        buttonDeleter.className = 'tasks_buttons';
+        buttonDeleter.id = 'delete_task';
+        buttonDeleter.textContent = 'Delete task';
+        notesContainer.append(buttonDeleter);
+
+        containerForDates.removeEventListener('click', createTaskList);
+        buttonCreator.addEventListener('click', writeYourTasks);
+        function writeYourTasks() {
+            let yourNewTask = document.createElement('input');
+            yourNewTask.className = 'input_for_new_task';
+            yourNewTask.id = uniqueId
+            tasksHolder.append(yourNewTask);
+        }
+
+        // let newNote = document.createElement('div');
+        // newNote.id = uniqueId;
+        // newNote.className = 'new_task_unique';
+        // newNote.textContent = 'Приветики';
+        // placeForTasks.append(newNote);
+        
+    } 
+} 
+
